beaf522cce79acfbd36b774be826265270729819
MarianaShebanova <mariana.shebanova@gmail.com>
2019-10-16 13:03:56 -0700

    JavaScript III

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When in the global scope, the value of “this” will be the window/console Object;
+* 2. Whenever a function is called by a preceding dot, the object before that dot is this.
+* 3. Whenever a constructor function is used, this refers to the specific instance of the object that is created and returned by the constructor function.
+* 4. Whenever JavaScript’s call or apply method is used, this is explicitly defined.
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +13,49 @@
 
 // code example for Window Binding
 
+function sayName(name) {
+    console.log(this);
+    return name;
+}
+sayName("D'Artagnan");
+
 // Principle 2
+
+const myObj = {
+    greeting: 'Hello',
+    sayHello: function (name) {
+        console.log(`${this.greeting} my name is ${name}`);
+        console.log(this);
+    }
+};
+myObj.sayHello('Ryan');
 
 // code example for Implicit Binding
 
 // Principle 3
+function CordialPerson(greeter) {
+    this.greeting = 'Hello ';
+    this.greeter = greeter;
+    this.speak = function () {
+        console.log(this.greeting + this.greeter);
+        console.log(this);
+    };
+}
+
+const jerry = new CordialPerson('Newman');
+const newman = new CordialPerson('Jerry');
+
+jerry.speak();
+newman.speak();
 
 // code example for New Binding
 
 // Principle 4
+
+function Ghost(saying){
+   this.phrase = saying;
+ }
+
+let myGhost = new Ghost('Casper the friendly 👻');
 
 // code example for Explicit Binding
